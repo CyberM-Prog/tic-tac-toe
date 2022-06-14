6f6885898fe611fa9dc3f2bb8e78c46dc38fa2cc
Martim <martim.pintao@gmail.com>
2022-06-14 13:28:10 +0100

    Create function that puts the markers on the screen when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,21 +1,40 @@
 const gameBoard = (() => {
-    const board = ["O", "X", "O", "O", "O", "X", "X", "O", "X"]
+    const board = ["", "", "", "", "", "", "", "", ""];
 
-    function renderBoard() {
+    (function renderBoard() {
         const boardContainer = document.querySelector(".gameboard")
 
         for (let i = 0; i < 9; i++) {
             const boardCell = document.createElement("div")
             boardCell.classList.add("cell")
+            boardCell.id = i
             boardCell.textContent = board[i]
             boardContainer.appendChild(boardCell)
+            console.log(boardCell.id)
         }
-    }
-    renderBoard()
+    })()
+
+    const cells = document.querySelectorAll(".cell")
+    
+    let i = 0
+    cells.forEach(cell => {
+        cell.addEventListener("click", function() {
+            const cellId = cell.id
+            if (i % 2 === 0) board[cellId] = game.player1.choice
+            else board[cellId] = game.player2.choice
+            this.textContent = board[cellId]
+            i++
+            console.log(board)
+        }, {once: true})
+    })
 
     return {board}
 })()
 
-const PlayerFactory = (name, choice) => {name, choice}
+const PlayerFactory = (name, choice) => ({name, choice})
 
-const game = () => {}
+const game = (() => {
+    const player1 = PlayerFactory("John", "X")
+    const player2 = PlayerFactory("Jane", "O")
+    return {player1, player2}
+})();
