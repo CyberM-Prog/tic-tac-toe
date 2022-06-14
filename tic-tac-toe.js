9bb6008782a5d1eba8ce5ea2aba85546a86dedd2
Martim <martim.pintao@gmail.com>
2022-06-14 09:55:37 +0100

    Create a function that shows the board array content on the page

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,18 @@
 const gameBoard = (() => {
-    let board = ["", "", "", "", "", "", "", "", ""]
+    const board = ["O", "X", "O", "O", "O", "X", "X", "O", "X"]
+
+    function renderBoard() {
+        const boardContainer = document.querySelector(".gameboard")
+
+        for (let i = 0; i < 9; i++) {
+            const boardCell = document.createElement("div")
+            boardCell.classList.add("cell")
+            boardCell.textContent = board[i]
+            boardContainer.appendChild(boardCell)
+        }
+    }
+    renderBoard()
+
     return {board}
 })()
 
